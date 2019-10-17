426064e9d659c2ecd7f7fa6e544254c785a6bf61
Stefan Lazovic <agios.stefanos@hotmail.com>
2019-10-17 14:52:26 +0200

    Updates

---
+++ precache-manifest.32a3efd777b04a43fdf5c0f576558586.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25f6e01e803aa512be3d471479e9865d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "9465cf2e550faca7dd63",
+    "url": "/monsters-rolodex/static/css/main.1a3d80b2.chunk.css"
+  },
+  {
+    "revision": "ca903edc950d52328219",
+    "url": "/monsters-rolodex/static/js/2.e124cbd2.chunk.js"
+  },
+  {
+    "revision": "9465cf2e550faca7dd63",
+    "url": "/monsters-rolodex/static/js/main.b7641fc9.chunk.js"
+  },
+  {
+    "revision": "d71975295fe45a6cbab0",
+    "url": "/monsters-rolodex/static/js/runtime-main.4686dfee.js"
+  }
+]);
